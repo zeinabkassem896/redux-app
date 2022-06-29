8527eaf82fa2df395a372344f8976b2687a082c5
zeinabkassem896 <zeinabkassem896@gmail.com>
2022-06-29 17:02:44 +0300

    adding product page2

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { InputGroup, Form, Container, Row, Col } from 'react-bootstrap';
+import { InputGroup, Form, Container, Row, Col, Dropdown, DropdownButton } from 'react-bootstrap';
 import '../css/Product.css';
 
 
@@ -8,12 +8,30 @@ function products(props) {
         <Container>
 
             <Row className="text-center">
-                <Col sm={3}>
+                <Col md={3}>
                     <InputGroup className="mb-3">
                         <InputGroup.Text id="basic-addon1">Search</InputGroup.Text>
                         <Form.Control className='search_input'
-                        placeholder="Product Name ..."
+                        placeholder="Product Name"
                         />
+                    </InputGroup>
+                </Col>
+                <Col md={6}></Col>
+                <Col lg={3} className=".align-self-start">
+                    <InputGroup className="mb-2">
+                    <DropdownButton
+          variant="outline-secondary"
+          title="Dropdown"
+          id="input-group-dropdown-1"
+        >
+          <Dropdown.Item href="#">Action</Dropdown.Item>
+          <Dropdown.Item href="#">Another action</Dropdown.Item>
+          <Dropdown.Item href="#">Something else here</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item href="#">Separated link</Dropdown.Item>
+        </DropdownButton>
+                        <InputGroup.Text id="basic-addon1">Filer By</InputGroup.Text>
+
                     </InputGroup>
                 </Col>
             </Row>
